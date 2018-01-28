3f0cfc9db22c4d2a67bcd872145cec4fba57a836
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-01-27 17:44:50 -0800

    Solution to day 19

--- day19.py
+++ day19.py
@@ -2,6 +2,9 @@
 
 from get_input import get_input, line_parser
 from itertools import cycle
+import logging
+
+log = logging.getLogger(__name__)
 
 def part1(n):
     l = 1
@@ -12,117 +15,54 @@ def part1(n):
 def test_part1():
     assert 3 == part1(5)
 
-# ?
-# ? k
-# x k ?
-# ? x k x
-# x ? k x x
-# x x ? k x x
-# x x x k ? x x
-# x x x x k x ? x
-
-# ?
-# ? k
-# x k ?
-# ? k x x
-# x k ? x x
-# x k x x ? x
-# x k x x x x ?
-# ? k x x x x x x
-# x k ? x x x x x x
-
-# ?
-# ? k
-# x k ?
-# ? k x x
-# x k ? x x
-# x k x ? x x
-# x k x x x ? x
-# ? k x x x x x x
-# x k ? x x x x x x
-
 def josephus(start, k_gen):
     survivor = 0
-    for n, k in zip(range(1, start+1), k_gen):
-        if survivor == k:
-            survivor += 1
-        lookup = { survivor: '?', k: 'k' }
-        print('{}: {}: {}'.format(survivor, k, ' '.join('x' if p not in lookup else lookup[p] for p in range(n))))
-        if k + 1 < survivor:
-            survivor = (survivor + 2) % n
+    for members, kill in zip(range(2, start+1), k_gen):
+        if kill - 1 <= survivor:
+            survivor = (survivor + 2) % members
         else:
-            survivor = (survivor + 1) % n
+            survivor = (survivor + 1) % members
+        # Skip string concatination unless needed, extremely slow
+        if log.level <= logging.DEBUG:
+            lookup = { survivor: '?', kill: 'k' }
+            log.debug('{}: {}: {}'.format(survivor, kill, 
+                ' '.join('x' if p not in lookup else lookup[p] 
+                    for p in range(1+members))))
     return survivor + 1
 
 def test_josephus():
-    assert 3 == josephus(9, cycle([1]))
-    for n in range(2, 20):
-        j = josephus(n, cycle([1]))
-        assert part1(n) == j
-        assert n == josephus(n, cycle([0]))
+    assert 3 == josephus(5, cycle([1]))
+    assert 2 == josephus(5, ((k // 2) + 1 for k in range(5-1)))
+    for i in range(3, 30):
+        assert brute(i, cycle([1])) == josephus(i, cycle([1]))
+        joseph_gen = lambda n: ((k // 2) + 1 for k in range(n-1))
+        brute_gen = lambda n: ((k // 2) + 1 for k in reversed(range(n-1)))
+        assert brute(i, brute_gen(i)) == josephus(i, joseph_gen(i))
 
-def brute2(n_elves, debug=False):
+def brute(n_elves, k_gen, debug=False):
     elves = list(range(1, n_elves+1))
     e = 0
-    while len(elves) > 1:
-        r = (e + len(elves) // 2) % len(elves)
-        if debug:
-            print('{:2},'.format(elves.pop(r)), end='')
-        else:
-            elves.pop(r)
-        if r > e:
-            e = (e + 1) % len(elves)
-    if debug:
-        print('{:2}'.format(elves[0]))
+    for kill in k_gen:
+        if len(elves) == 1:
+            break
+        log.debug('{}: {}'.format(kill, elves))
+        elves.pop(kill)
+        elves.append(elves.pop(0))
+    assert len(elves) != 0
     return elves[0]
 
+def test_brute():
+    assert 3 == brute(5, cycle([1]))
+    assert 2 == brute(5, ((k // 2) + 1 for k in reversed(range(5-1))))
+    assert 3 == brute(6, ((k // 2) + 1 for k in reversed(range(6-1))))
+    assert 5 == brute(7, ((k // 2) + 1 for k in reversed(range(7-1))))
+
 def part2(n):
-    kills = [(k // 2) + 1 for k in range(n-1)]
+    kills = ((k // 2) + 1 for k in range(n-1))
     return josephus(n, kills)
 
-def test_part2():
-    assert 1 == brute2(2)
-    assert 3 == brute2(3)
-    assert 1 == part2(4)
-    assert 1 == brute2(4)
-    assert 2 == brute2(5)
-
 if __name__ == '__main__':
+    log.setLevel(logging.INFO)
     lines = int(get_input(day=19, year=2016).strip())
     print("Part 1: {}".format(part1(lines)))
     print("Part 2: {}".format(part2(lines)))
-
-# 1 2 3 4
-# 1 2   4
-# 1 2    
-# 1
-
-# 1 2 3 4 5
-# 1 2   4 5
-# 1 2   4  
-#   2   4  
-#   2      
-
-# 1 2 3 4 5 6
-# 1 2 3   5 6
-# 1 2 3     6
-#   2 3     6
-#     3     6
-#     3
-
-# 1 2 3 4 5 6 7
-# 1 2 3   5 6 7
-# 1 2 3   5   7
-# 1 2 3   5
-# 1   3   5
-# 1       5
-#         5
-
-# 1 2 3 4 5 6 7 8
-# 1 2 3 4   6 7 8
-# 1 2 3 4     7 8
-# 1 2 3 4     7  
-#   2 3 4     7  
-#   2   4     7  
-#   2         7  
-#             7  
