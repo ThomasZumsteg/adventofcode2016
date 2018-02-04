0b763750ce6a090ad9a2b046c1c431627762086b
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-02-03 22:33:35 -0800

    Solution to day 22

--- day22.py
+++ day22.py
@@ -6,17 +6,8 @@ from itertools import product
 from collections import namedtuple
 import re
 
-class Node(object):
-    def __init__(self, x, y, size, used, *args):
-        self.inital = (x, y)
-        self.x = x
-        self.y = y
-        self.size = size
-        self.used = used
-
-    @property
-    def avail(self):
-        return self.size - self.used
+Node = namedtuple('Node', 'x y size used avail p_used'.split())
+Point = namedtuple('Point', ['x', 'y'])
 
 def part1(nodes):
     pairs = set()
@@ -27,18 +18,40 @@ def part1(nodes):
     return len(pairs)
 
 def part2(nodes, upper=100):
-    print(build(nodes, upper))
-    # queue = [(0, zero, state)]
-    # seen = set()
-    # while queue:
-    #     steps, (zx, zy), state = queue.pop(0)
-    #     if state in seen:
-    #         continue
-    #     for mx, my in [(zx+1, zy), (zx-1, zy), 
-    #             (zx, zy+1), (zx, zy-1)]:
-    #         if not (0 <= my < len(state)) 
+    (max_x, max_y), zero, full_nodes = build(nodes, upper)
+    queue = [(0, zero, (max_x, 0))]
+    seen = set()
+    while queue:
+        steps, zero, goal = queue.pop(0)
+        if goal == Point(0, 0):
+            return steps
+        if (zero, goal) in seen:
+            continue
+        seen.add((zero, goal))
+        for p in [Point(zero.x+dx, zero.y+dy) for dx, dy in \
+                [(0,1),(0,-1),(1,0),(-1,0)]]:
+            if p in full_nodes or \
+                not (0 <= p.x <= max_x) or not (0 <= p.y <= max_y):
+                continue
+            if p == goal:
+                queue.append((steps+1, p, zero))
+            else:
+                queue.append((steps+1, p, goal))
 
-    #     print(row)
+TEST_TEXT = """Filesystem            Size  Used  Avail  Use%
+/dev/grid/node-x0-y0   10T    8T     2T   80%
+/dev/grid/node-x0-y1   11T    6T     5T   54%
+/dev/grid/node-x0-y2   32T   28T     4T   87%
+/dev/grid/node-x1-y0    9T    7T     2T   77%
+/dev/grid/node-x1-y1    8T    0T     8T    0%
+/dev/grid/node-x1-y2   11T    7T     4T   63%
+/dev/grid/node-x2-y0   10T    6T     4T   60%
+/dev/grid/node-x2-y1    9T    8T     1T   88%
+/dev/grid/node-x2-y2    9T    6T     3T   66%"""
+
+def test_part2():
+    nodes = line_parser(TEST_TEXT, parse=parse, numbered=True)
+    assert part2(nodes, upper=25) == 7
 
 def build(nodes, full_limit):
     max_x, max_y = 0, 0
@@ -46,9 +59,9 @@ def build(nodes, full_limit):
     for n in nodes:
         max_x, max_y = max(max_x, n.x), max(max_y, n.y)
         if n.used == 0:
-            zero = (n.x, n.y)
+            zero = Point(n.x, n.y)
         elif n.used > full_limit:
-            full_nodes.add((n.x, n.y))
+            full_nodes.add(Point(n.x, n.y))
     return (max_x, max_y), zero, full_nodes
 
 def parse(line, i):
